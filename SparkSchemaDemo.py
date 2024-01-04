70599a23b41e6fe732d4fbcde38af6330f70e07f
Audrey457 <laudrey457@gmail.com>
2024-01-04 16:47:09 +0100

    schema

--- SparkSchemaDemo.py
+++ SparkSchemaDemo.py
@@ -1,4 +1,5 @@
 from pyspark.sql import *
+from pyspark.sql.types import *
 
 from lib.logger import Log4J
 from lib.utils import *
@@ -11,11 +12,36 @@ if __name__ == "__main__":
 
     logger = Log4J(spark)
 
-    flightTimeCsvDF = get_raw_df_from_csv(spark, "data/flight*.csv")
+    flightSchemaStruct = StructType([
+        StructField("FL_DATE", DateType()),
+        StructField("OP_CARRIER", StringType()),
+        StructField("OP_CARRIER_FL_NUM", IntegerType()),
+        StructField("ORIGIN", StringType()),
+        StructField("ORIGIN_CITY_NAME", StringType()),
+        StructField("DEST", StringType()),
+        StructField("DEST_CITY_NAME", StringType()),
+        StructField("CRS_DEP_TIME", IntegerType()),
+        StructField("DEP_TIME", IntegerType()),
+        StructField("WHEELS_ON", IntegerType()),
+        StructField("TAXI_IN", IntegerType()),
+        StructField("CRS_ARR_TIME", IntegerType()),
+        StructField("ARR_TIME", IntegerType()),
+        StructField("CANCELLED", IntegerType()),
+        StructField("DISTANCE", IntegerType()),
+    ])
+
+    flightSchemaDDL = ("FL_DATE DATE,OP_CARRIER STRING,OP_CARRIER_FL_NUM INT, "
+                       "ORIGIN STRING,ORIGIN_CITY_NAME STRING,"
+                       "DEST STRING,DEST_CITY_NAME STRING,"
+                       "CRS_DEP_TIME INT,DEP_TIME INT,WHEELS_ON INT,"
+                       "TAXI_IN INT,CRS_ARR_TIME INT,ARR_TIME INT,"
+                       "CANCELLED INT,DISTANCE INT")
+
+    flightTimeCsvDF = get_raw_df_from_csv(spark, "data/flight*.csv", flightSchemaStruct)
     flightTimeCsvDF.show(5)
     logger.info("CSV schema: " + flightTimeCsvDF.schema.simpleString())
 
-    flightTimeJsonDF = get_raw_df_from_json(spark, "data/flight*.json")
+    flightTimeJsonDF = get_raw_df_from_json(spark, "data/flight*.json", flightSchemaDDL)
     flightTimeJsonDF.show(5)
     logger.info("Json schema: " + flightTimeJsonDF.schema.simpleString())
 
